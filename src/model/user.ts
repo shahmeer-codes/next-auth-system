3f7377ea0a9ae867f98f0b4d70f6c1c191220250
shahmeer <ranashahmeerarshad@gmail.com>
2026-04-12 01:07:38 -0700

    make database schema

---
+++ src/model/user.ts
@@ -0,0 +1,43 @@
+import mongoose from "mongoose";
+
+interface user_type{
+    _id?:mongoose.Types.ObjectId;
+    name:string;
+    image:string;
+    email:string;
+    password:string;
+    createdAt:Date;
+    updatedAt:Date;
+}
+
+const user_schema=new mongoose.Schema<user_type>({
+     name: {
+        type: String,
+        required: true
+    },
+    image: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now
+    },
+    updatedAt: {
+        type: Date,
+        default: Date.now
+    }
+}, 
+{ timestamps: true });
+
+const user=mongoose.model("user",user_schema)
+export default user
